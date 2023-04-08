67e3c1dccfeb57de31ec8a1dac5b3425d571809d
Pal Paypal <pdangelo1988@gmail.com>
2023-04-07 22:10:04 -0400

    another corrected commit

--- src/pages/about-me.tsx
+++ src/pages/about-me.tsx
@@ -13,7 +13,7 @@ const aboutSections = [
     }, {
         heading: 'UCF Bootcamp',
         paragraphs: [
-            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe nemo architecto quas fuga velit optio voluptatibus excepturi iusto rerum, numquam deserunt similique a? Doloremque unde, velit excepturi quas delectus magnam.',
+            'lLorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe nemo architecto quas fuga velit optio voluptatibus excepturi iusto rerum, numquam deserunt similique a? Doloremque unde, velit excepturi quas delectus magnam.',
             'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe nemo architecto quas fuga velit optio voluptatibus excepturi iusto rerum, numquam deserunt similique a? Doloremque unde, velit excepturi quas delectus magnam.',
             'Lorem ipsum dolor, sit amet consectetur ptatibus excepturi iusto rerum, numquam deserunt similique a? Doloremque unde, velit excepturi quas delectus magnam.'
         ]
